5ce2192e5734a6266af409b2bab19a458e71ef87
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-09 19:22:00 +0530

    Create XPattern.java

---
+++ XPattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class XPattern{
+  public static void main(String[]args){
+    System.out.print("********X PATTERN********");
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the value of n to terminate:");
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++){
+       for(int j=1;j<=n;j++){
+         if(i==j||j==n-i+1){
+           System.out.print("*");
+         }
+         else{
+           System.out.print(" ");
+         }
+       }
+      System.out.println();
+    }
+  }
+}
+
+      
