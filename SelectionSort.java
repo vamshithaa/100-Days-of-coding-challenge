3c121e943359568e89b1e1947dc9d2ed7de3e81f
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-10 20:39:29 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SelectionSort{
+public class Main{
   public static void main(String[]args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the array size:");
@@ -11,7 +11,7 @@ public class SelectionSort{
     }
     System.out.print("Array before sorting:");
     for(int i=0; i<n;i++){
-      System.out.print(a[i]);
+      System.out.print(a[i]+" ");
     }
      System.out.println();
     System.out.print("Array after sorting:");
@@ -26,6 +26,10 @@ public class SelectionSort{
         a[j]=temp;
       }
     }
+    for(int i=0; i<n;i++){
+        System.out.print(a[i]+" ");
+    }
+  
   }
 }
       
