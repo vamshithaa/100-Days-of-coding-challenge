74dbd43d7bb510ef56cc3804f326c679933f2db1
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-13 19:52:23 +0530

    Create ButterflyPatt.java

---
+++ ButterflyPatt.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class ButterflyPatt{
+    public static void main(String[]args){
+        System.out.println("Enter the value of n:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+         for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+
+    }
+}
