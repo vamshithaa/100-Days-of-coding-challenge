ea5277b436a5d4e9b4008c7a6058943d8af124b8
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-13 18:54:41 +0530

    Create FloydsTriPatt.java

---
+++ FloydsTriPatt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FloydsTriPatt{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of n:");
+    int n=sc.nextInt();
+    int num=1;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print(num+" ");
+        num++;
+      }
+      System.out.println();
+    }
+  }
+}
+
+    
