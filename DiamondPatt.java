7596fa4fa5db78f63c89118ae3a7828646394128
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-14 18:40:10 +0530

    Update DiamondPatt.java

--- DiamondPatt.java
+++ DiamondPatt.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class DaimondPatt{
+public class DiamondPatt{
   public static void main(String[]args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the value of n:");
@@ -9,11 +9,19 @@ public class DaimondPatt{
         System.out.print(" ");
       }
       for(int j=1;j<=i;j++){
-        System.out.print("*");
+        System.out.print("* ");
+      }
+     
+      System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
       }
       for(int j=1;j<=i;j++){
-        System.out.print("*");
+        System.out.print("* ");
       }
+     
       System.out.println();
     }
   }
