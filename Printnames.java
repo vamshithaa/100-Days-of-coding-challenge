7df6dfe45bf3cc7c675cb2878ee471f82a9a65d3
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-21 20:51:45 +0530

    Update Printnames.java

--- Printnames.java
+++ Printnames.java
@@ -1,19 +1,21 @@
 import java.util.*;
-public class Printnames{
-  public static void main(String[]args){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the size of the array:");
-    int n=sc.nextInt();
-    String arr[]=new String[n];
-    System.out.print("Enter the names:");
-    for(int i=0;i<n;i++){
-      arr[i]=sc.nextLine();
-    }
-    System.out.print("The names present in the array is:");
-    for(int i=0;i<n;i++){
-      System.out.println(arr[i]);
-    }
-  }
-}
 
-      
+public class Printnames {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        sc.nextLine(); 
+        String[] arr = new String[n];
+        
+        System.out.println("Enter the names:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+        
+        System.out.println("The names present in the array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
