a8e91f0d61bd44b210c6fba19000cfad05735eb4
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-01-28 20:34:08 +0530

    Update FindOccurance.java

--- FindOccurance.java
+++ FindOccurance.java
@@ -1,10 +1,11 @@
+import java.util.*;
 public class FindOccurance{
 public static int first=-1;
 public static int last=-1;
 public static void Find(String str, int indx, char element){
   if(indx==str.length()){
-    System.out.println(first);
-    System.out.println(last);
+    System.out.println("The first occurance of element "+element+" is at index:"+first);
+    System.out.println("The last occurance of element "+element+" is at index:"+last);
     return;
   }
   char currentelement=str.charAt(indx);
@@ -19,8 +20,15 @@ public static void Find(String str, int indx, char element){
   Find(str,indx+1, element);
 }
   public static void  main(String[]args){
-    String str="aa";
-    Find(str,0,'a');
+    Scanner sc=new Scanner(System.in);
+    System.out.println("****Finding the first and last occurance of an element in the given string****"); 
+    System.out.println("Enter the string:");
+    String str=sc.nextLine();
+    System.out.println("Enter the element to find its 1st and last occurance from the given string:");
+    char element=sc.next().charAt(0);
+    Find(str,0,element);
   }
 }
     
+
+  
