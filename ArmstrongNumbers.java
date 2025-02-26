793ee581d0e82b4274134113cca91d3c51418098
Vamshitha R Poojary <154661821+vamshithaa@users.noreply.github.com>
2025-02-26 12:25:27 +0530

    Update ArmstrongNumbers.java

--- ArmstrongNumbers.java
+++ ArmstrongNumbers.java
@@ -1,23 +1,23 @@
-public class ArmstrongNumbers
-{
-public static void main(String[] args)
-{
-int num,rem,limit=1000, sum = 0;
-System.out.print("Armstrong numbers from 1 to N:");
-for (int i = 1; i <= limit; i++)
-{
-num = i;
-while (num > 0)
-{
-rem = num % 10;
-sum = sum + (rem*rem*rem);
-num = num / 10;
-}
-if (sum == i)
-{
-System.out.print(i + " ");
-}
-sum = 0;
-}
-}
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        int num, rem, limit = 1000, sum;
+
+        System.out.println("Armstrong numbers from 1 to 1000:");
+
+        for (int i = 1; i <= limit; i++) {
+            num = i;
+            sum = 0; // Reset sum for each number
+
+            while (num > 0) {
+                rem = num % 10;
+                sum += (rem * rem * rem);
+                num /= 10;
+            }
+
+            if (sum == i) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
 }
